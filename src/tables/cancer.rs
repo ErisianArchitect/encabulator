83ef876e50cb2b60c27919cf5a23fa5878cd3e70
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-02-26 18:33:16 -0800

    Needs some more work.

--- src/tables/cancer.rs
+++ src/tables/cancer.rs
@@ -1,11 +1,17 @@
 //! The table in this module defines the minimum donation amount per year for various levels of Bromite.
 //! There is also a lookup function which will safely interpolate between Bromite levels to give you the
-//! most fair minimum donation amount.
+//! most fair minimum donation amount (in USD). This table does not account for inflation, and was
+//! created some time around February of 2026.
 
 /// (Bromite levels in cubic meters)
 pub const TABLE: &[(f64, u64)] = &[
     /// If there is 0 Bromite, you shouldn't donate any money because that would be waste. You should
     /// spend that money on Thorium for your phase selectors.
-    (0.0f64, 0),
-    
+    (0.0, 0),
+    /// Most of your workers will get cancers, especially colon cancer. $10 should be good enough.
+    (1.0, 10),
+    /// You will get cancer.
+    (10.0, 10),
+    /// This is enough to Bromite to give cancer to 100,000 Resus Monkeys.
+    (500.0, 11),
 ];
